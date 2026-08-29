2bacf778a845888b38bae3062c290574112ffa72
raphael AUDONNET <raphafoot42@gmail.com>
2026-08-29 11:36:34 +0200

    Fix import statement formatting in login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router' 
 import Link from 'next/link'
 import { createClient } from '@/lib/supabase-client'
 
